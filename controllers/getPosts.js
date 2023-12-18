c14e2f4a3cf71bfca1c79cc358f297d5c233d38c
Chirag Sharma <128295328+k1ttu@users.noreply.github.com>
2023-12-19 02:18:08 +0530

    commit-x

--- controllers/getPosts.js
+++ controllers/getPosts.js
@@ -1,6 +1,6 @@
 export default async function getPosts1(){
     try{
-        const response = await fetch("http://localhost:3000/api/posts",
+        const response = await fetch("https://blog2-6defb4hr6-k1ttus-projects.vercel.app/api/posts",
         {
             cache:'no-store'
         }
--- controllers/getSinglePost.js
+++ controllers/getSinglePost.js
@@ -1,6 +1,6 @@
 export default async function getSinglePost1(id){
     try{
-        const response= await fetch(`http://localhost:3000/api/posts/${id}`, {
+        const response= await fetch(`https://blog2-6defb4hr6-k1ttus-projects.vercel.app/api/posts/${id}`, {
             cache:'no-store'
         })
         const post = await response.json();
